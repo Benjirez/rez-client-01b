a69fcb5e2b11d66431fd36826ba355b325740fce
Benjirez <benjirez@gmail.com>
2024-03-28 12:59:48 -0700

    changed to API_URI

--- src/stateStore.js
+++ src/stateStore.js
@@ -8,7 +8,7 @@ export const collPick = writable(0)
 
 export const test1 = writable('test')
 
-export const API_URI = writable('https://rez-new-tab-04.onrender.com') 
+export const API_URI = writable('https://aware-crow-shrug.cyclic.app') 
 
 //export const API_URI = writable('http://localhost:3000')
 
