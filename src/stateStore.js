155bca14095829352b559ba5b258b4070e465905
Benjirez <benjirez@gmail.com>
2024-05-23 15:03:26 -0700

    Update stateStore.js

--- src/stateStore.js
+++ src/stateStore.js
@@ -8,7 +8,7 @@ export const collPick = writable(0)
 
 export const selectedIndex = writable('none')
 
-export const API_URI = writable('https://rez-api-01.vercel.app') 
+export const API_URI = writable('https://rez-api-01.vercel.app/') 
 
 //export const API_URI = writable('http://localhost:3000')
 
