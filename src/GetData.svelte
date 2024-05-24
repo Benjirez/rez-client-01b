a8d5b2d600d86b670282866dd36ff78cbe52f0c3
Benjirez <benjirez@gmail.com>
2024-05-24 12:02:45 -0700

    Update GetData.svelte

--- src/GetData.svelte
+++ src/GetData.svelte
@@ -32,21 +32,3 @@
     <PopupMessage>Waiting...</PopupMessage>
   {/if}
 
-<!-- PopupMessage.svelte -->
-<style>
-  .popup {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: #fff;
-    padding: 20px;
-    border: 1px solid #ccc;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
-    z-index: 9999;
-  }
-</style>
-
-<div class="popup">
-  <slot></slot>
-</div>
